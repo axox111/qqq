de9d428baa3efa2981c7414821134624203b0056
Vladislav <gggwp4@gmail.com>
2023-01-20 21:56:58 +0300

    +full insert

--- java/LinkedList/src/linkedlist/LinkedList.java
+++ java/LinkedList/src/linkedlist/LinkedList.java
@@ -3,23 +3,58 @@ public class LinkedList {
     
     Node head = null;
     
-    public void insert(Node val){
+    public void insert(Node val) {
+        
         Node new_node = val;
-        Node last = this.head;
-        if(last == null){
-            last = new_node;
+        if(this.head == null) {
+            this.head = new_node;
         } else {
-            last.next = new_node;
-            new_node = last; 
+            Node islast = this.head;
+            while(islast.next != null) {
+                islast = islast.next;
+                }
+            islast.next = new_node;
         }
+    }
+    
+    public void insert(Node val, int pos) {
+        
+        Node new_node = val;
+        Node node = this.head;
+        int i = 1;
+        int size = this.getSize();
+        if (0 > pos | pos > size) {
+            System.out.println("Position out of list size");
+        } else if (pos == 0) {
+            new_node.next = node;
+            this.head = new_node;
+        } else {
+            while (i != pos) {
+                node = node.next;
+                i = i + 1;
+            } 
+            new_node.next = node.next;
+            node.next = new_node;
+        }
+    }  
+    
+    public int getSize() {
+        Node islast = this.head;
+        int i = 1;
+        while (islast.next != null) {
+            islast = islast.next;
+            i++;
+        }
+        return i;
     }
     
     public void getList() {
         Node first = this.head;
-        while(first != null){
-            System.out.println(first.val);
+        while(first.next != null) {
+            System.out.print(first.val + ", ");
             first = first.next; 
         }
+        System.out.println(first.val);
     }
 
     public static void main(String[] args) {
@@ -27,9 +62,14 @@ public class LinkedList {
         Node a = new Node(7); 
         Node b = new Node(14); 
         Node c = new Node(73); 
+        Node d = new Node(56);
+        Node g = new Node(2456);
         ll.insert(a);
-//       ll.insert(b);
-//        ll.insert(c);
+        ll.insert(b);
+        ll.insert(c);
+        ll.insert(d);
+        ll.insert(g,5);
+        ll.getSize();
         ll.getList();
     }
 }
---
+++ java/LinkedList/src/linkedlist/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedlist;
+
+/**
+ *
+ * @author user
+ */
+public class NewClass {
+    public static void main(String [] args){
+        int a = 4;
+        if (a < -1 | a > 10  ){
+            System.out.println("меньше нуля или больше размера списка");
+         
+          
+            
+        }else{
+            System.out.println("vse ok");
+        }
+    }
+}
